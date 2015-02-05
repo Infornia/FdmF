5048a324ed374f07e44dc1a0dbe99f09be132e82
Michael WILK <mwilk@e3r6p8.42.fr>
2015-02-05 19:19:26 +0100

    Add zoom

---
+++ ft_fdf_zoom.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_fdf_zoom.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mwilk <mwilk@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/02/04 17:07:41 by mwilk             #+#    #+#             */
+/*   Updated: 2015/02/04 17:50:31 by mwilk            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fdf.h"
+
+void	more_zoom(t_data *d)
+{
+	d->zoom += 0.1;
+	erase_img(d);
+}
+
+void	less_zoom(t_data *d)
+{
+	d->zoom -= 0.1;
+	erase_img(d);
+}
+
+void	more_peak(t_data *d)
+{
+	d->size_peaks += 1;
+	erase_img(d);
+}
+
+void	less_peak(t_data *d)
+{
+	d->size_peaks -= 1;
+	erase_img(d);
+}
